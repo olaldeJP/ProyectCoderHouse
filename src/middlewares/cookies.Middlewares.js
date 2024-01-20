d42bde34709d09e066bb5f8d94fb790161f46343
olaldeJP <olaldejp@gmail.com>
2024-01-20 10:13:36 -0300

    update

--- src/middlewares/cookies.Middlewares.js
+++ src/middlewares/cookies.Middlewares.js
@@ -1,12 +1,7 @@
 import { encriptar } from "../utils/criptorafia.js";
 import { COOKIE_OPTS } from "../config/config.js";
 import { desencriptar } from "../utils/criptorafia.js";
-export async function cookieCreate(req, res, next) {
-  res.cookie("cookie", "Esta Cookie es re power", {
-    maxAge: 1000 * 60 * 60 * 24,
-  });
-  next();
-}
+
 export async function guardarUserToken(req, res, next) {
   const accessToken = await encriptar(req.user);
   res.cookie("authorization", accessToken, COOKIE_OPTS);
--- static/js/register.js
+++ static/js/register.js
@@ -3,7 +3,6 @@ const enviarR = document.querySelector("#enviarRegistro");
 
 formReg?.addEventListener("submit", async (event) => {
   event.preventDefault();
-
   //Se envia la peticion POST con la informacion del formulario y se redirige al home con el res.session['usser] {first_name , last_name. isAdmin}
   const response = await fetch("/api/users/", {
     method: "POST",
